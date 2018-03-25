1124306f61162d3c29f6b2fd6f429f9ee90bcc04
kenjiro310 <jhrucker@gmail.com>
2018-03-24 21:11:50 -0400

    Android Life CYcle CSRPSD
    MainActivity.java
    (50, 12) // TODO (1) Use logAndAppend within onCreate
    (54, 8) // TODO (2) Override onStart, call super.onStart, and call logAndAppend with ON_START
    (68, 8) // TODO (3) Override onResume, call super.onResume, and call logAndAppend with ON_RESUME
    (83, 8) // TODO (4) Override onPause, call super.onPause, and call logAndAppend with ON_PAUSE
    (100, 8) // TODO (5) Override onStop, call super.onStop, and call logAndAppend with ON_STOP
    (117, 8) // TODO (6) Override onRestart, call super.onRestart, and call logAndAppend with ON_RESTART
    (130, 8) // TODO (7) Override onDestroy, call super.onDestroy, and call logAndAppend with ON_DESTROY

--- Exercises-student/Lesson05a-Android-Lifecycle/T05a.01-Exercise-LogLifecycle/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Exercises-student/Lesson05a-Android-Lifecycle/T05a.01-Exercise-LogLifecycle/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -48,19 +48,98 @@ public class MainActivity extends AppCompatActivity {
         mLifecycleDisplay = (TextView) findViewById(R.id.tv_lifecycle_events_display);
 
         // TODO (1) Use logAndAppend within onCreate
+        logAndAppend(ON_CREATE);
     }
 
     // TODO (2) Override onStart, call super.onStart, and call logAndAppend with ON_START
+    /**
+     * Called when the activity is becoming visible to the user.
+     *
+     * Followed by onResume() if the activity comes to the foreground, or onStop() if it becomes
+     * hidden.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        logAndAppend(ON_START);
+    }
 
     // TODO (3) Override onResume, call super.onResume, and call logAndAppend with ON_RESUME
+    @Override
+    /**
+     * Called when the activity will start interacting with the user. At this point your activity
+     * is at the top of the activity stack, with user input going to it.
+     *
+     * Always followed by onPause().
+     */
+    protected void onResume() {
+        super.onResume();
+
+        logAndAppend(ON_RESUME);
+    }
+
 
     // TODO (4) Override onPause, call super.onPause, and call logAndAppend with ON_PAUSE
+    @Override
+    /**
+     * Called when the system is about to start resuming a previous activity. This is typically
+     * used to commit unsaved changes to persistent data, stop animations and other things that may
+     * be consuming CPU, etc. Implementations of this method must be very quick because the next
+     * activity will not be resumed until this method returns.
+     *
+     * Followed by either onResume() if the activity returns back to the front, or onStop() if it
+     * becomes invisible to the user.
+     */
+    protected void onPause() {
+        super.onPause();
+
+        logAndAppend(ON_PAUSE);
+    }
 
     // TODO (5) Override onStop, call super.onStop, and call logAndAppend with ON_STOP
+    @Override
+    /**
+     * Called when the activity is no longer visible to the user, because another activity has been
+     * resumed and is covering this one. This may happen either because a new activity is being
+     * started, an existing one is being brought in front of this one, or this one is being
+     * destroyed.
+     *
+     * Followed by either onRestart() if this activity is coming back to interact with the user, or
+     * onDestroy() if this activity is going away.
+     */
+    protected void onStop() {
+        super.onStop();
+
+        logAndAppend(ON_STOP);
+    }
 
     // TODO (6) Override onRestart, call super.onRestart, and call logAndAppend with ON_RESTART
+    /**
+     * Called after your activity has been stopped, prior to it being started again.
+     *
+     * Always followed by onStart()
+     */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        logAndAppend(ON_RESTART);
+    }
 
     // TODO (7) Override onDestroy, call super.onDestroy, and call logAndAppend with ON_DESTROY
+    /**
+     * The final call you receive before your activity is destroyed. This can happen either because
+     * the activity is finishing (someone called finish() on it, or because the system is
+     * temporarily destroying this instance of the activity to save space. You can distinguish
+     * between these two scenarios with the isFinishing() method.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        logAndAppend(ON_DESTROY);
+    }
 
     /**
      * Logs to the console and appends the lifecycle method name to the TextView so that you can
